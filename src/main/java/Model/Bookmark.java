4f1533c22fc455462cece0610680dc399deab7f5
Joel Brandao <joelsantosbrandao@gmail.com>
2022-04-11 16:40:30 +0100

    projeto

---
+++ src/main/java/Model/Bookmark.java
@@ -0,0 +1,16 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bookmark {
+
+        private List<String> url;
+        private List<String> tag;
+
+        public Bookmark() {
+            url=new ArrayList<>();
+        }
+
+
+}
