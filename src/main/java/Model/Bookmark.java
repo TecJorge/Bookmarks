f90ec8f51976af721c24040fa368c34cc5f31985
Joel Brandao <joelsantosbrandao@gmail.com>
2022-04-11 16:45:59 +0100

    classe bookmark

--- src/main/java/Model/Bookmark.java
+++ src/main/java/Model/Bookmark.java
@@ -4,13 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bookmark {
+    private String nome;
+    private List<String> url;
+    private List<String> tag;
 
-        private List<String> url;
-        private List<String> tag;
+    public Bookmark(String nome) {
+        this.nome=nome;
+        url = new ArrayList<>();
+        tag = new ArrayList<>();
+    }
 
-        public Bookmark() {
-            url=new ArrayList<>();
-        }
+    public List<String> getUrl() {
+        return url;
+    }
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<String> getTag() {
+        return tag;
+    }
+
+    public void setUrl(List<String> url) {
+        this.url = url;
+    }
+
+    public void setTag(List<String> tag) {
+        this.tag = tag;
+    }
+
+    @Override
+    public String toString() {
+        return
+        String.format("Bookmark: %s\nUrl´s:\n%s\nTag´s:\n%s\n",nome,url,tag);
+    }
+    
 }
